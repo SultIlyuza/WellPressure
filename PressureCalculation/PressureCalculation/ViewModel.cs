4e8ef65a632a95263fe8dfc0209a8716ab2ec01e
SultIlyuza <sultangulova98@mail.ru>
2019-10-22 17:47:59 +0500

    commit

---
+++ PressureCalculation/PressureCalculation/ViewModel.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Windows.Controls.DataVisualization.Charting;
+
+namespace PressureCalculation
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Well> Wells { get; set; }
+        private Well selectedWell;
+        private string pauseText;
+
+        public double depth { get; set; }
+        public double density  {get; set; }
+        public int numOfSteps { get; set; }
+
+        private bool IsPause = false;
+        private bool IsCalcStart = false;
+        int index;
+        int id;
+
+        
+        ManualResetEvent me = new ManualResetEvent(false);
+        ManualResetEvent me1 = new ManualResetEvent(false);
+
+        public ViewModel()
+        {
+            Wells = new ObservableCollection<Well>();
+            id = 1;
+        }
+
+        public Well SelectedWell
+        {
+            get { return selectedWell; }
+            set
+            {
+                selectedWell = value;
+                OnPropertyChanged("SelectedWell");
+            }
+        }
+
+        public string PauseText
+        {
+            get { return pauseText; }
+            set
+            {
+                pauseText = value;
+                OnPropertyChanged("PauseText");
+            }
+        }
+
+        private ICommand _add;
+        public ICommand AddCommand
+        {
+
+            get { 
+                return _add ?? (_add = new RelayCommand(o =>
+                {
+                    Wells.Add(new Well {Id = id, Depth = depth, DensityOfLiquid = density, NumberOfSteps = numOfSteps });
+                    id++;
+                }));
+            }
+        }
+
+        private ICommand _calculate;
+        public ICommand CalculateCommand
+        {
+            get 
+            {
+                return _calculate ?? (_calculate = new RelayCommand(o =>
+                {
+                    CalculatePessure();
+                }));
+            }
+        }
+
+        private async void CalculatePessure()
+        {
+            await Task.Run(() =>
+            {
+                IsCalcStart = true;
+                foreach (var w in Wells)
+                    if (!IsPause)
+                    {
+                        w.CalculatePressure();
+                        index = w.Id;
+                        if (IsPause)
+                            me1.Set();
+                    }
+                    else
+                    {
+                        me.WaitOne();
+                        w.CalculatePressure();
+                        index = w.Id;
+                    }
+                IsCalcStart = false;
+            });
+        }
+
+        private ICommand _pause;
+        public ICommand PauseCommand
+        {
+            get
+            {
+                return _pause ?? (_pause = new RelayCommand(o =>
+                {
+                    if (IsCalcStart)
+                    {
+                        if (!IsPause)
+                        {
+                            IsPause = true;
+                            me1.WaitOne();
+                            PauseText = "Расчет остановлен\nСкважина " + index + ", \nГлубина " + Wells[index - 1].Pressures.Last().Key + ", \nДавление " + Wells[index - 1].Pressures.Last().Value;
+                        }
+                        else
+                        {
+                            IsPause = false;
+                            me.Set();
+                            PauseText = "Расчет возобновлен";
+                        }
+                    }
+                }));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+}
---
+++ PressureCalculation/PressureCalculation/Well.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace PressureCalculation
+{
+    public class Well
+    {
+        private const double g = 9.08665;
+
+        private int id;
+        private double depth;
+        private double densityOfLiquid;
+        private int numberOfSteps;
+        
+        List<KeyValuePair<double, double>> pressures;
+
+        public KeyValuePair<double, double> keyValuePair;
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        public double Depth
+        {
+            get { return depth; }
+            set
+            {
+                depth = value;
+                OnPropertyChanged("Depth");
+            }
+        }
+        public double DensityOfLiquid
+        {
+            get
+            {
+                return densityOfLiquid;
+            }
+            set
+            {
+                densityOfLiquid = value;
+                OnPropertyChanged("DensityOfLiquid");
+            }
+        }
+        public int NumberOfSteps
+        {
+            get { return numberOfSteps; }
+            set
+            {
+                numberOfSteps = value;
+                OnPropertyChanged("NumberOfSteps");
+            }
+        }
+
+        public List<KeyValuePair<double, double>> Pressures
+        {
+            get { return pressures; }
+            set
+            {
+                pressures = value;
+                OnPropertyChanged("Pressures");
+            }
+        }
+
+
+        public  void CalculatePressure()
+        {
+            pressures = new List<KeyValuePair<double, double>>();
+            keyValuePair = new KeyValuePair<double, double>();
+            double interval;
+            double pressure;
+
+            for (int i = 0; i < numberOfSteps; i++)
+            {
+                interval = (depth / numberOfSteps) * (i + 1);
+                pressure = densityOfLiquid * g * interval;
+                pressures.Add(new KeyValuePair<double, double>(interval, pressure));
+                Thread.Sleep(100);
+
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
